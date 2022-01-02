fdb8144aa2c694be1c5b283b6cf76bfbf20e8630
PeterT <tranpp87@gmail.com>
2022-01-02 15:13:29 -0500

    update navbar border and background color for each section

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
               <div className='picture-wrapper'></div>
                 <div className='title'>
                   <h3>Software Engineer | Mechanical Engineer</h3>
-                    <p>I am Full Stack Developer with Project Management experience. I have developed web applications using languages such as JavaScript and Python along with frameworks including Express.js and Django. </p> <br />
+                    <p>I am Full Stack Developer with 8+ years Automotive Project Management experience. I have developed web applications using languages such as JavaScript and Python along with frameworks including Express.js and Django. </p> <br />
                     <p>When i'm not tinkering on projects, engaging on coding challenges or learning something new, you can find me tearing up the bowling alley's with my average score of 💯, on the 🏀 court chasing bricks or watching my new-found sport, ⛳️ 🏌🏼‍♂️. </p>
                 </div>
         </div>
