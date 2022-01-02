079fc4caeff320efb0848912620a8cc2a6e9def7
PeterT <tranpp87@gmail.com>
2022-01-02 13:20:40 -0500

    add site links and github links to projects

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -23,41 +23,57 @@ const Projects = () => {
               <img src={blackJack} alt="blackjack screen shot"/>
               <div className='description'>
                 <h2>Casual BlackJack</h2>
-                <p>Looking to kill some time or can't get enough BlackJack?</p>
+                <p>BlackJack game that features DOM manipulation and light/dark mode.</p>
                 <div className='technologies'>
                   <SiJavascript /> <AiFillHtml5 />  <DiCss3 /> <FaBootstrap />
                 </div>
               </div>
+                <div className='project-links'>
+                  <a href="https://letsplayjsblackjack.netlify.app/" target='_blank' rel="noreferrer"><button>Site Link</button></a>
+                  <a href="https://github.com/PPT87/BlackJack" target='_blank' rel="noreferrer"><button>GitHub Link</button></a>
+                </div>
             </div>
             <div className='project-card'>
               <img src={chefskiss} alt="Chef's Kiss screen shot"/>
               <div className='description'>
                 <h2>Chef's Kiss</h2>
-                <p>Maybe that dish deserves a second chance...or not.</p>
+                <p>Full CRUD food rating and review app. Requires Google Oauth to use.</p>
                 <div className='technologies'>
                   <SiMongodb /> <SiExpress />  <FaNodeJs />
                 </div>
               </div>
+              <div className='project-links'>
+                  <a href="https://chefs-kiss-app.herokuapp.com/" target='_blank' rel="noreferrer"><button>Site Link</button></a>
+                  <a href="https://github.com/PPT87/Chefs-Kiss-Food-Review" target='_blank' rel="noreferrer"><button>GitHub Link</button></a>
+                </div>
             </div>
             <div className='project-card'>
               <img src={jotdown} alt="JotDown screen shot"/>
               <div className='description'>
                 <h2>JotDown</h2>
-                <p>Never forget what you're thinking or need to do.</p>
+                <p>Todo app to quickly write down tasks or build a list. Requires login to use.</p>
                 <div className='technologies'>
                   <FaPython /> <DiDjango />  <SiPostgresql /> <FaBootstrap />
                 </div>
               </div>
+              <div className='project-links'>
+                  <a href="https://jotdowntodoapp.herokuapp.com/" target='_blank' rel="noreferrer"><button>Site Link</button></a>
+                  <a href="https://github.com/PPT87/jot_down" target='_blank' rel="noreferrer"><button>GitHub Link</button></a>
+                </div>
             </div>
             <div className='project-card'>
               <img src={allthefeels} alt="AllTheFeels screen shot"/>
               <div className='description'>
                 <h2>AllTheFeels</h2>
-                <p>Relax through art and share with the community.</p>
+                <p>Targeting adolescent mental health by uploading art to express their feelings.</p>
                 <div className='technologies'>
                   <SiMongodb /> <SiExpress />  <SiReact /> <FaNodeJs />
                 </div>
               </div>
+              <div className='project-links'>
+                  <a href="https://allthefeelsapp.herokuapp.com/" target='_blank' rel="noreferrer"><button>Site Link</button></a>
+                  <a href="https://github.com/PPT87/allTheFeels" target='_blank' rel="noreferrer"><button>GitHub Link</button></a>
+                </div>
             </div>
           </div>
         </div>
