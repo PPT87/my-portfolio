c19b739444b613939e10d6c830dda8d01c559365
PeterT <tranpp87@gmail.com>
2022-01-02 12:51:21 -0500

    add resume download to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,6 +6,8 @@ import { CgProfile } from 'react-icons/cg'
 import { GiSkills } from 'react-icons/gi'
 import { RiContactsBookLine } from 'react-icons/ri'
 import { AiOutlineFileDone } from 'react-icons/ai'
+import { IoDocumentTextSharp } from 'react-icons/io5'
+import resume from '../../Assets/Peter-Tran-Resume.pdf'
 import '../Navbar/Navbar.css'
 
 
@@ -25,6 +27,7 @@ const Navbar = ({ showNav, setShowNav }) => {
           <li><Link to='about' smooth={true} ><CgProfile />About Me</Link></li>
           <li><Link to='skills' smooth={true} ><GiSkills />Skills</Link></li>
           <li><Link to='projects' smooth={true} ><AiOutlineFileDone />Projects</Link></li>
+          <li><a href={resume} download ><IoDocumentTextSharp />Resume</a></li>
           <li><Link to='contact' smooth={true} ><RiContactsBookLine />Contact</Link></li>
         </ul>
     </nav>
